0f5202cec728468e056820319ef22b2f505d5f09
Fukuiyuan <FuKuiyuan@outlook.com>
2021-04-11 19:49:14 +0800

    24

---
+++ .leetcode/24.swap-nodes-in-pairs.py
@@ -0,0 +1,87 @@
+#
+# @lc app=leetcode id=24 lang=python3
+#
+# [24] Swap Nodes in Pairs
+#
+# https://leetcode.com/problems/swap-nodes-in-pairs/description/
+#
+# algorithms
+# Medium (53.48%)
+# Likes:    3491
+# Dislikes: 209
+# Total Accepted:    596.2K
+# Total Submissions: 1.1M
+# Testcase Example:  '[1,2,3,4]'
+#
+# Given a linked list, swap every two adjacent nodes and return its head.
+#
+#
+# Example 1:
+#
+#
+# Input: head = [1,2,3,4]
+# Output: [2,1,4,3]
+#
+#
+# Example 2:
+#
+#
+# Input: head = []
+# Output: []
+#
+#
+# Example 3:
+#
+#
+# Input: head = [1]
+# Output: [1]
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the list is in the range [0, 100].
+# 0 <= Node.val <= 100
+#
+#
+#
+# Follow up: Can you solve the problem without modifying the values in the
+# list's nodes? (i.e., Only nodes themselves may be changed.)
+#
+# @lc idea=start
+#
+# 反转相邻的两个节点。直接操作就行。
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        pseudo = ListNode()
+        pseudo.next = head
+        p = pseudo
+        while p.next:
+            if p.next.next:
+                l = p.next
+                r = l.next
+                l.next = r.next
+                r.next = l
+                p.next = r
+                p = l
+            else:
+                break
+        return pseudo.next
+
+# @lc code=end
