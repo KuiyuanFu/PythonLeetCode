656adcaa8c896eb5988b2f669f75e4932d825c76
Fukuiyuan <FuKuiyuan@outlook.com>
2021-04-11 20:39:52 +0800

    25

---
+++ .leetcode/25.reverse-nodes-in-k-group.py
@@ -0,0 +1,122 @@
+#
+# @lc app=leetcode id=25 lang=python3
+#
+# [25] Reverse Nodes in k-Group
+#
+# https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+#
+# algorithms
+# Hard (45.33%)
+# Likes:    3501
+# Dislikes: 400
+# Total Accepted:    342.2K
+# Total Submissions: 753.9K
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# Given a linked list, reverse the nodes of a linked list k at a time and
+# return its modified list.
+#
+# k is a positive integer and is less than or equal to the length of the linked
+# list. If the number of nodes is not a multiple of k then left-out nodes, in
+# the end, should remain as it is.
+#
+# Follow up:
+#
+#
+# Could you solve the problem in O(1) extra memory space?
+# You may not alter the values in the list's nodes, only nodes itself may be
+# changed.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [2,1,4,3,5]
+#
+#
+# Example 2:
+#
+#
+# Input: head = [1,2,3,4,5], k = 3
+# Output: [3,2,1,4,5]
+#
+#
+# Example 3:
+#
+#
+# Input: head = [1,2,3,4,5], k = 1
+# Output: [1,2,3,4,5]
+#
+#
+# Example 4:
+#
+#
+# Input: head = [1], k = 1
+# Output: [1]
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the list is in the range sz.
+# 1 <= sz <= 5000
+# 0 <= Node.val <= 1000
+# 1 <= k <= sz
+#
+#
+#
+# @lc idea=start
+#
+# 反转相邻的 n 个节点，需要二次遍历
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if k == 1:
+            return head
+
+        pseudo = ListNode()
+        pseudo.next = head
+        # p 是上一组的最后一个
+        p = pseudo
+        while p.next and p.next.next:
+            # 看是否够 k个
+            t = p
+            for _ in range(k):
+                t = t.next
+                if not t:
+                    return pseudo.next
+
+            # 反转
+            pNext = p.next
+            l = pNext
+            r = l.next
+            rNext = r.next
+            for _ in range(1, k):
+                rNext = r.next
+                r.next = l
+                l = r
+                r = rNext
+                
+
+            p.next = l
+            pNext.next = r
+            p = pNext
+
+        return pseudo.next
+# @lc code=end
+
