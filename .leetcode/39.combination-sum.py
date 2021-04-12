67eb9002edd4aba8ad63010c9ffadad03e4a4d23
Fukuiyuan <FuKuiyuan@outlook.com>
2021-04-12 17:40:18 +0800

    39, 40

---
+++ .leetcode/39.combination-sum.py
@@ -0,0 +1,117 @@
+#
+# @lc app=leetcode id=39 lang=python3
+#
+# [39] Combination Sum
+#
+# https://leetcode.com/problems/combination-sum/description/
+#
+# algorithms
+# Medium (59.69%)
+# Likes:    5744
+# Dislikes: 150
+# Total Accepted:    714.2K
+# Total Submissions: 1.2M
+# Testcase Example:  '[2,3,6,7]\n7'
+#
+# Given an array of distinct integers candidates and a target integer target,
+# return a list of all unique combinations of candidates where the chosen
+# numbers sum to target. You may return the combinations in any order.
+#
+# The same number may be chosen from candidates an unlimited number of times.
+# Two combinations are unique if the frequency of at least one of the chosen
+# numbers is different.
+#
+# It is guaranteed that the number of unique combinations that sum up to target
+# is less than 150 combinations for the given input.
+#
+#
+# Example 1:
+#
+#
+# Input: candidates = [2,3,6,7], target = 7
+# Output: [[2,2,3],[7]]
+# Explanation:
+# 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple
+# times.
+# 7 is a candidate, and 7 = 7.
+# These are the only two combinations.
+#
+#
+# Example 2:
+#
+#
+# Input: candidates = [2,3,5], target = 8
+# Output: [[2,2,2,2],[2,3,3],[3,5]]
+#
+#
+# Example 3:
+#
+#
+# Input: candidates = [2], target = 1
+# Output: []
+#
+#
+# Example 4:
+#
+#
+# Input: candidates = [1], target = 1
+# Output: [[1]]
+#
+#
+# Example 5:
+#
+#
+# Input: candidates = [1], target = 2
+# Output: [[1,1]]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= candidates.length <= 30
+# 1 <= candidates[i] <= 200
+# All elements of candidates are distinct.
+# 1 <= target <= 500
+#
+#
+#
+#
+#
+# @lc idea=start
+#
+# 求数字和为target 的所有组合。递归，备忘录。
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort(True)
+        self.candidates = candidates
+        self.memo = {}
+        return self.do(0, target)
+
+    def do(self, index, target):
+
+        if (index, target) in self.memo:
+            return self.memo[(index, target)]
+        n = target // self.candidates[index]
+        result = []
+        if target % self.candidates[index] == 0:
+            result.append([self.candidates[index]]*n)
+            n -= 1
+        if index + 1 < len(self.candidates):
+            for i in range(n+1):
+                for t in self.do(index+1, target - self.candidates[index] * i):
+                    result.append([self.candidates[index]]*i + t)
+        return result
+
+
+# @lc code=end
+if __name__ == '__main__':
+    print(Solution().combinationSum([2, 3, 6, 7],  7))
---
+++ .leetcode/40.combination-sum-ii.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode id=40 lang=python3
+#
+# [40] Combination Sum II
+#
+# https://leetcode.com/problems/combination-sum-ii/description/
+#
+# algorithms
+# Medium (50.39%)
+# Likes:    2646
+# Dislikes: 86
+# Total Accepted:    400.8K
+# Total Submissions: 794.7K
+# Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+#
+# Given a collection of candidate numbers (candidates) and a target number
+# (target), find all unique combinations in candidates where the candidate
+# numbers sum to target.
+#
+# Each number in candidates may only be used once in the combination.
+#
+# Note: The solution set must not contain duplicate combinations.
+#
+#
+# Example 1:
+#
+#
+# Input: candidates = [10,1,2,7,6,1,5], target = 8
+# Output:
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
+#
+#
+# Example 2:
+#
+#
+# Input: candidates = [2,5,2,1,2], target = 5
+# Output:
+# [
+# [1,2,2],
+# [5]
+# ]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= candidates.length <= 100
+# 1 <= candidates[i] <= 50
+# 1 <= target <= 30
+#
+#
+#
+
+# @lc idea=start
+#
+# 求数字和为target 的所有组合。递归，备忘录。
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        d = {}
+        for w in candidates:
+            d[w] = d.get(w, 0) + 1
+
+        self.dic = d
+        self.candidates = list(d.keys())
+        self.memo = {}
+        return self.do(0, target)
+
+    def do(self, index, target):
+
+        if (index, target) in self.memo:
+            return self.memo[(index, target)]
+        n = target // self.candidates[index]
+        n = n if n < self.dic[self.candidates[index]
+                              ] else self.dic[self.candidates[index]]
+        result = []
+        if target == self.candidates[index] * n:
+            result.append([self.candidates[index]]*n)
+            n -= 1
+        if index + 1 < len(self.candidates):
+            for i in range(n+1):
+                for t in self.do(index+1, target - self.candidates[index] * i):
+                    result.append([self.candidates[index]]*i + t)
+        return result
+
+
+# @lc code=end
+if __name__ == '__main__':
+    print(Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
