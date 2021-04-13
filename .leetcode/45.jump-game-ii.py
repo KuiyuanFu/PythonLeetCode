e1c8a959cfb0c13ffa0c859144ed1d1546d42ac3
Fukuiyuan <FuKuiyuan@outlook.com>
2021-04-13 14:59:46 +0800

    45

---
+++ .leetcode/45.jump-game-ii.py
@@ -0,0 +1,94 @@
+#
+# @lc app=leetcode id=45 lang=python3
+#
+# [45] Jump Game II
+#
+# https://leetcode.com/problems/jump-game-ii/description/
+#
+# algorithms
+# Medium (31.68%)
+# Likes:    3942
+# Dislikes: 176
+# Total Accepted:    329.1K
+# Total Submissions: 1M
+# Testcase Example:  '[2,3,1,1,4]'
+#
+# Given an array of non-negative integers nums, you are initially positioned at
+# the first index of the array.
+#
+# Each element in the array represents your maximum jump length at that
+# position.
+#
+# Your goal is to reach the last index in the minimum number of jumps.
+#
+# You can assume that you can always reach the last index.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [2,3,1,1,4]
+# Output: 2
+# Explanation: The minimum number of jumps to reach the last index is 2. Jump 1
+# step from index 0 to 1, then 3 steps to the last index.
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [2,3,0,1,4]
+# Output: 2
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 1000
+# 0 <= nums[i] <= 10^5
+#
+#
+#
+#
+#
+# @lc idea=start
+#
+# 给定数组，数组的数字就是当前的跳跃能力，目标是跳到数组的结尾。
+# 求最小的跳跃次数，贪心算法，每次跳跃看跳到当前跳跃范围内的格子中，再跳一次，最远跳跃距离，来确定下一次跳到哪。
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
+        times = 0
+        now = 0
+        while True:
+            jumpLength = nums[now]
+            if now + jumpLength >= len(nums)-1:
+                times += 1
+                break
+            next = now
+            maxLength = 0
+            for i in range(now + 1, now + 1+jumpLength):
+                if i + nums[i] > maxLength:
+                    next = i
+                    maxLength = i + nums[i]
+
+            times += 1
+            now = next
+
+        return times
+
+
+# @lc code=end
+if __name__ == '__main__':
+    print(Solution().jump([2, 3, 1, 1, 4]))
+    print(Solution().jump([2, 3, 0, 1, 4]))
+    print(Solution().jump([2]))
