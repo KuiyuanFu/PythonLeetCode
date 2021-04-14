721c8e76cd1184044fee3851a80ceb34579aa4e1
Fukuiyuan <FuKuiyuan@outlook.com>
2021-04-14 22:25:46 +0800

    62

---
+++ .leetcode/62.unique-paths.py
@@ -0,0 +1,99 @@
+#
+# @lc app=leetcode id=62 lang=python3
+#
+# [62] Unique Paths
+#
+# https://leetcode.com/problems/unique-paths/description/
+#
+# algorithms
+# Medium (56.30%)
+# Likes:    4903
+# Dislikes: 241
+# Total Accepted:    624.9K
+# Total Submissions: 1.1M
+# Testcase Example:  '3\n7'
+#
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+# the diagram below).
+#
+# The robot can only move either down or right at any point in time. The robot
+# is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+# the diagram below).
+#
+# How many possible unique paths are there?
+#
+#
+# Example 1:
+#
+#
+# Input: m = 3, n = 7
+# Output: 28
+#
+#
+# Example 2:
+#
+#
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the
+# bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+#
+#
+# Example 3:
+#
+#
+# Input: m = 7, n = 3
+# Output: 28
+#
+#
+# Example 4:
+#
+#
+# Input: m = 3, n = 3
+# Output: 6
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= m, n <= 100
+# It's guaranteed that the answer will be less than or equal to 2 * 10^9.
+#
+#
+#
+#
+#
+# @lc idea=start
+#
+# 在一个board 上，只能向右或下走，从左上到右下有多少种路径。
+# dp。
+#
+# @lc idea=end
+
+from typing import *
+from collections import *
+
+
+# @lc code=start
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * m]*2
+        for _ in range(n - 1):
+            dp[1], dp[0] = dp[0], dp[1]
+            for j in range(1, m):
+                dp[1][j] = dp[0][j] + dp[1][j-1]
+        return dp[1][-1]
+
+
+# @lc code=end
+
+if __name__ == '__main__':
+    print(Solution().uniquePaths(2,2))
+    print(Solution().uniquePaths(2,3))
+    print(Solution().uniquePaths(3,3))
+    print(Solution().uniquePaths(3, 7))
