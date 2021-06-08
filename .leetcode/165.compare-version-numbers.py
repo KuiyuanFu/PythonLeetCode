3ca455739e7bb04ad7bdcf0216de65aabc09b810
Fukuiyuan <FuKuiyuan@outlook.com>
2021-06-08 12:41:33 +0800

    165

---
+++ .leetcode/165.compare-version-numbers.py
@@ -0,0 +1,186 @@
+# @lc app=leetcode id=165 lang=python3
+#
+# [165] Compare Version Numbers
+#
+# https://leetcode.com/problems/compare-version-numbers/description/
+#
+# algorithms
+# Medium (30.93%)
+# Likes:    833
+# Dislikes: 1712
+# Total Accepted:    245.7K
+# Total Submissions: 792.8K
+# Testcase Example:  '"1.01"\n"1.001"'
+#
+# Given two version numbers, version1 and version2, compare them.
+#
+#
+#
+#
+# Version numbers consist of one or more revisions joined by a dot '.'. Each
+# revision consists of digits and may contain leading zeros. Every revision
+# contains at least one character. Revisions are 0-indexed from left to right,
+# with the leftmost revision being revision 0, the next revision being revision
+# 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
+#
+# To compare version numbers, compare their revisions in left-to-right order.
+# Revisions are compared using their integer value ignoring any leading zeros.
+# This means that revisions 1 and 001 are considered equal. If a version number
+# does not specify a revision at an index, then treat the revision as 0. For
+# example, version 1.0 is less than version 1.1 because their revision 0s are
+# the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
+#
+# Return the following:
+#
+#
+# If version1 < version2, return -1.
+# If version1 > version2, return 1.
+# Otherwise, return 0.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: version1 = "1.01", version2 = "1.001"
+# Output: 0
+# Explanation: Ignoring leading zeroes, both "01" and "001" represent the same
+# integer "1".
+#
+#
+# Example 2:
+#
+#
+# Input: version1 = "1.0", version2 = "1.0.0"
+# Output: 0
+# Explanation: version1 does not specify revision 2, which means it is treated
+# as "0".
+#
+#
+# Example 3:
+#
+#
+# Input: version1 = "0.1", version2 = "1.1"
+# Output: -1
+# Explanation: version1's revision 0 is "0", while version2's revision 0 is
+# "1". 0 < 1, so version1 < version2.
+#
+#
+# Example 4:
+#
+#
+# Input: version1 = "1.0.1", version2 = "1"
+# Output: 1
+#
+#
+# Example 5:
+#
+#
+# Input: version1 = "7.5.2.4", version2 = "7.5.3"
+# Output: -1
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= version1.length, version2.length <= 500
+# version1 and version2 only contain digits and '.'.
+# version1 and version2 are valid version numbers.
+# All the given revisions in version1 and version2 can be stored in a 32-bit
+# integer.
+#
+#
+#
+
+# @lc tags=string
+
+# @lc imports=start
+from imports import *
+
+# @lc imports=end
+
+# @lc idea=start
+#
+# 给定两个版本号，判断大小关系。
+# 版本号使用“.”来进行连接的。
+#
+# @lc idea=end
+
+# @lc group=
+
+# @lc rank=
+
+
+# @lc code=start
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        vs1 = version1.split('.')
+        vs2 = version2.split('.')
+        cl = min(len(vs1), len(vs2))
+        for i in range(cl):
+            if int(vs1[i]) < int(vs2[i]):
+                return -1
+            elif int(vs1[i]) > int(vs2[i]):
+                return 1
+        flag = 1
+        if len(vs1) < len(vs2):
+            flag = -1
+            vs1, vs2 = vs2, vs1
+        for i in range(cl, len(vs1)):
+            if int(vs1[i]) != 0:
+                return flag
+        return 0
+
+
+# @lc code=end
+
+# @lc main=start
+if __name__ == '__main__':
+    print('Example 1:')
+    print('Input : ')
+    print('version1 = "1.01", version2 = "1.001"')
+    print('Exception :')
+    print('0')
+    print('Output :')
+    print(str(Solution().compareVersion("1.01", "1.001")))
+    print()
+
+    print('Example 2:')
+    print('Input : ')
+    print('version1 = "1.0", version2 = "1.0.0"')
+    print('Exception :')
+    print('0')
+    print('Output :')
+    print(str(Solution().compareVersion("1.0", "1.0.0")))
+    print()
+
+    print('Example 3:')
+    print('Input : ')
+    print('version1 = "0.1", version2 = "1.1"')
+    print('Exception :')
+    print('-1')
+    print('Output :')
+    print(str(Solution().compareVersion("0.1", "1.1")))
+    print()
+
+    print('Example 4:')
+    print('Input : ')
+    print('version1 = "1.0.1", version2 = "1"')
+    print('Exception :')
+    print('1')
+    print('Output :')
+    print(str(Solution().compareVersion("1.0.1", "1")))
+    print()
+
+    print('Example 5:')
+    print('Input : ')
+    print('version1 = "7.5.2.4", version2 = "7.5.3"')
+    print('Exception :')
+    print('-1')
+    print('Output :')
+    print(str(Solution().compareVersion("7.5.2.4", "7.5.3")))
+    print()
+
+    pass
+# @lc main=end
