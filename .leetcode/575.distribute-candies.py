dd88e4106032ca7cb87e54d8a06214f4238d71d8
KuiyuanFu <fukuiyuan@outlook.com>
2021-09-28 02:56:58 +0800

    575

---
+++ .leetcode/575.distribute-candies.py
@@ -0,0 +1,122 @@
+# @lc app=leetcode id=575 lang=python3
+#
+# [575] Distribute Candies
+#
+# https://leetcode.com/problems/distribute-candies/description/
+#
+# algorithms
+# Easy (64.94%)
+# Likes:    827
+# Dislikes: 1079
+# Total Accepted:    182.6K
+# Total Submissions: 280.8K
+# Testcase Example:  '[1,1,2,2,3,3]'
+#
+# Alice has n candies, where the i^th candy is of type candyType[i]. Alice
+# noticed that she started to gain weight, so she visited a doctor.
+#
+# The doctor advised Alice to only eat n / 2 of the candies she has (n is
+# always even). Alice likes her candies very much, and she wants to eat the
+# maximum number of different types of candies while still following the
+# doctor's advice.
+#
+# Given the integer array candyType of length n, return the maximum number of
+# different types of candies she can eat if she only eats n / 2 of them.
+#
+#
+# Example 1:
+#
+#
+# Input: candyType = [1,1,2,2,3,3]
+# Output: 3
+# Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3
+# types, she can eat one of each type.
+#
+#
+# Example 2:
+#
+#
+# Input: candyType = [1,1,2,3]
+# Output: 2
+# Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types
+# [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+#
+#
+# Example 3:
+#
+#
+# Input: candyType = [6,6,6,6]
+# Output: 1
+# Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2
+# candies, she only has 1 type.
+#
+#
+#
+# Constraints:
+#
+#
+# n == candyType.length
+# 2 <= n <= 10^4
+# n is even.
+# -10^5 <= candyType[i] <= 10^5
+#
+#
+#
+
+# @lc tags=hash-table
+
+# @lc imports=start
+from imports import *
+
+# @lc imports=end
+
+# @lc idea=start
+#
+# 判断类型。
+#
+# @lc idea=end
+
+# @lc group=
+
+# @lc rank=
+
+
+# @lc code=start
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(set(candyType)), len(candyType) // 2)
+
+
+# @lc code=end
+
+# @lc main=start
+if __name__ == '__main__':
+    print('Example 1:')
+    print('Input : ')
+    print('candyType = [1,1,2,2,3,3]')
+    print('Exception :')
+    print('3')
+    print('Output :')
+    print(str(Solution().distributeCandies([1, 1, 2, 2, 3, 3])))
+    print()
+
+    print('Example 2:')
+    print('Input : ')
+    print('candyType = [1,1,2,3]')
+    print('Exception :')
+    print('2')
+    print('Output :')
+    print(str(Solution().distributeCandies([1, 1, 2, 3])))
+    print()
+
+    print('Example 3:')
+    print('Input : ')
+    print('candyType = [6,6,6,6]')
+    print('Exception :')
+    print('1')
+    print('Output :')
+    print(str(Solution().distributeCandies([6, 6, 6, 6])))
+    print()
+
+    pass
+# @lc main=end
